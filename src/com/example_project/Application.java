7781c5e77acf4c8adb5651855c0ffe027ba8d5ae
Renusree <renusreemalapati.com>
2026-03-18 10:10:50 +0530

    Added login feature

--- src/com/example_project/Application.java
+++ src/com/example_project/Application.java
@@ -1,5 +1,9 @@
 package com.example_project;
 
 public class Application {
+	public static void main(String[] args) {
+		System.out.println("Git Project");
+		System.out.println("Login feature");
+	}
 
 }
